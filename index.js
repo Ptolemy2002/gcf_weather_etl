2fbfd2b0ff15b4765cb7366b2c4a5a7352196d18
Ptolemy Henson <pbhenson@iu.edu>
2024-02-05 20:58:14 +0000

    Version 2

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+const {Storage} = require("@google-cloud/storage");
+const csv = require("csv-parser");
+
 exports.readObservation = (file, context) => {
-    console.log(`  Event: ${context.eventId}`);
-    console.log(`  Event Type: ${context.eventType}`);
-    console.log(`  Bucket: ${file.bucket}`);
-    console.log(`  File: ${file.name}`);
+    // console.log(`  Event: ${context.eventId}`);
+    // console.log(`  Event Type: ${context.eventType}`);
+    // console.log(`  Bucket: ${file.bucket}`);
+    // console.log(`  File: ${file.name}`);
+
+    const gcs = new Storage();
+    const dataFile = gcs.bucket(file.bucket).file(file.name);
+
+    dataFile.createReadStream()
+        .on("error", (e) => {
+            console.error(e);
+        })
+        .pipe(csv())
+            .on("data", (row) => {
+                console.log(row);
+            })
+            .on("end", () => {
+                console.log("End!")
+            });
 }
